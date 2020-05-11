c51d44f9a5ba26db07aa1fa98fe0cd17d12be609
ROHIT SINGLA <rohitsingla2611@gmail.com>
2020-05-11 07:42:57 +0530

    functions

--- functions.js
+++ functions.js
@@ -9,12 +9,25 @@ console.log("\n");
 cubeNumber(3);
 function cubeNumber(numberGet) {
 
-    return console.log("Cube of " + numberGet + " is " + numberGet ** 3);
+    return console.log("Cube of " , numberGet , " is " , numberGet ** 3);
 
 }
 
+const sum = (number11, number22) => number11 <= number22?[number11]:[number22] ;
+var sumResult = sum(11,26);
+console.log(sumResult);
+console.log(sumResult[0]);
+let number11 = 11;
+let number22 = 26;
+const biggest = (function (a,b){ //Immediately invoked functional expressions
+
+    let res;
+    a <= b? res = [a]:res = [b];
+return res;
+})(number11,number22);
 
 
+console.log("Immediately invoked function",biggest);
 
 // Arrow Functions 
 
